fb2fa72f7b16e3ef7d75a8947fb1b275792767cf
OU Soben <ousoben20@gmail.com>
2024-01-03 22:15:44 +0700

    This is the about us page

--- src/App.js
+++ src/App.js
@@ -11,25 +11,64 @@ function App() {
          </div>
     </div>
     <div className="container ">
-      <div className="row">
+      <div className="row py-5">
         <div className="col-lg-12 text-center col-xl-6">
           <h1 className='py-3'>About us Sport store</h1>
         <p className='p-5 '>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam totam eos eligendi eaque, beatae quos debitis soluta unde ratione molestiae molestias fuga, ex voluptatum accusamus doloremque odit dolorum obcaecati vitae.
           Iusto nobis totam suscipit hic dicta deserunt. Distinctio aut enim natus nisi magni, dolorum est nostrum excepturi minus quis, dolore culpa doloremque ut vero laboriosam minima id totam illum consectetur.
         </p></div>
-        <div className="col-md-12 col-xl-6 " > 
+        <div className="col-md-12 col-xl-6  " > 
         <img className='img-fluid rounded ' src="https://www.datacube.ae/static/about-us-ca5d21dd1cef3f0315b902b086eabf9f.jpg" alt="" /></div>
       </div>
       <div className="row mt-5">
          <div className="slider-image col-md-12 col-lg-12 col-xl-6  order-1 d-flex  ">
-          <img className='img-fluid container ' src="https://t4.ftcdn.net/jpg/02/61/52/95/360_F_261529596_YZWJaMnYFSCM0FSCrxs71o6RrZ9MpP4D.jpg" alt="" />
+          <img className='img-fluid container' src="https://t4.ftcdn.net/jpg/02/61/52/95/360_F_261529596_YZWJaMnYFSCM0FSCrxs71o6RrZ9MpP4D.jpg" alt="" />
          </div>
-         <div className="py-5 text-center col-xl-6 order-xl-2  ">
+         <div className="py-5 text-center col-xl-6 order-xl-2 py-5">
               <h1>Our Mission</h1>
               <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam repellendus sed, saepe tenetur recusandae deserunt quisquam veritatis quas natus, veniam distinctio, qui optio obcaecati! Magnam dolorum consequuntur sequi maxime facilis.
               Aperiam saepe unde veritatis magni, labore fuga placeat laudantium? Quis est non quia officia dicta sequi ducimus. Dignissimos eum, ut commodi nesciunt ratione vel totam? Nihil, alias. Deserunt, facilis animi.</p>
          </div>
+      </div>
+      <div className="row container pt-5">
+        <div className="text-center  py-5 col-xl-6">
+          <h1 className='p-5'>Our Team</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam maiores expedita, corrupti ipsa explicabo tempore eveniet natus. Illo voluptate sequi quos dolore. Nihil ratione atque culpa sint ex quam aliquid.
+          Autem esse rem sequi architecto dolorum eos reiciendis quo accusamus voluptatum quisquam itaque magnam totam, dolorem voluptates temporibus necessitatibus, quos delectus? Facilis quidem cumque quaerat inventore labore eveniet dignissimos architecto!</p>
+        </div>
+         <div className="col-lg-12 col-xl-6 col-lg-6 container">
+          <img className='img-fluid rounded container' src="https://chchurches.org/wp-content/uploads/2017/10/Fotolia_119039468_Subscription_Monthly_M.jpg" alt="" />
+         </div>
+      </div>
+      <div className="row py-5">
+        <div className=" pt-5 col-xl-3 order-1 ">
+              <img className="img-fluid rounded" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg/773px-Elon_Musk_Royal_Society_%28crop2%29.jpg" alt=""  />
+        </div>
+        <div className=" col-xl-8  container pt-5 mt-5 order-xl-2 px-5 col-xs-pe-4">
+          <h1>Mr.Elon Musk</h1>
+          <p className="fs-4">CEO,Founder</p>
+          <hr />
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim beatae fuga corrupti, exercitationem aut voluptatum similique voluptatibus numquam iure odit vel ipsam temporibus qui commodi? Eligendi esse minima id magnam.
+          Unde officia ipsam, alias beatae illo ducimus tempora accusamus eaque atque nesciunt ratione voluptatibus, hic modi. Consectetur tempore culpa distinctio asperiores? Ea asperiores facilis est provident optio dicta commodi nemo.</p>
+          <h1>FOLLOW ME</h1>
+          <hr />
+        </div>
+      </div>
+      <div className="row">
+        <div className="container pt-5 col-xl-8 px-5">
+          <h1>Mr.Ou Soben</h1>
+          <p className="fs-4">Developer</p>
+          <hr />
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga laborum quas, dolore praesentium, accusamus veritatis labore doloribus corrupti optio ipsa quia dolor quibusdam autem, voluptates quisquam? Id magni magnam nihil?
+          Nobis quisquam voluptatem expedita velit itaque dolorum aspernatur, aliquam ea delectus cum ut impedit distinctio omnis numquam in iure labore? Et ad quod ut perferendis laudantium enim autem consequatur? Sequi?
+          Voluptatum, aut ullam quae ratione, vero at perferendis earum ea aliquid, facere totam illum! Iure dolores, mollitia vero magnam consectetur optio vitae laborum aspernatur repellendus at aut corporis aperiam natus?</p>
+          <h1>FOLLOW ME</h1>
+          <hr />
+        </div>
+        <div className="col-xl-3 me-5 ">
+              <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" className="img-fluid rounded" />
+        </div>
       </div>
     </div>
     </>
